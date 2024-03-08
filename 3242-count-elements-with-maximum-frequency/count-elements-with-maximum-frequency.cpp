fedcb23b28daf8a7a108bc84d38e34caa877b14d
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-03-08 09:35:21 +0530

    Time: 6 ms (46.85%) | Memory: 22.1 MB (14.32%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,10 +1,13 @@
+static const int _ = []() {ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); return 0;}();
+
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-        vector<int> v(101,0);
-        for(auto &i:nums) v[i]++;
-        int m=*max_element(v.begin(),v.end());
-        int x=count(v.begin(),v.end(),m);
-        return x*m;
+        vector<int> v(101, 0);
+        for (auto& i : nums)
+            v[i]++;
+        int m = *max_element(v.begin(), v.end());
+        int x = count(v.begin(), v.end(), m);
+        return x * m;
     }
 };
