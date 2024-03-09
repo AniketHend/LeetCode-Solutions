7e528902522c5cfafbc8d03944a456c9ed8cf4eb
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-03-09 17:22:06 +0530

    Time: 22 ms (99.87%) | Memory: 52.7 MB (85.74%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
         int i=0,j=0;
         while(i<nums1.size() && j<nums2.size()) {
             if(nums1[i] < nums2[j]) i++;
