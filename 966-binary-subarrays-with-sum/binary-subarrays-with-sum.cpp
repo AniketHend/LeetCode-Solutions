641f870de3303ddf590d4ce2c741689e6e0f1c7d
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-03-14 12:49:41 +0530

    Time: 39 ms (52.67%) | Memory: 38.3 MB (20.69%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int k) {
-        map<int,int> m;
-        int c=0;
-        int sa=0;
-        int n=nums.size();
-        m[0]=1;
-        for(int i=0;i<n;i++) {
-            c+=nums[i];
-            if(m.find(c-k)!=m.end()) sa+=m[c-k];
+        unordered_map<int, int> m;
+        int c = 0;
+        int sa = 0;
+        int n = nums.size();
+        m[0] = 1;
+        for (int i = 0; i < n; i++) {
+            c += nums[i];
+            if (m.find(c - k) != m.end())
+                sa += m[c - k];
             m[c]++;
         }
         return sa;
