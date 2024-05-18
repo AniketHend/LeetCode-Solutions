f40844162280c4f1263fc6b65e55c749c68c8583
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-05-18 18:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (46.35%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,23 +1,38 @@
-int dp[102][2];
 class Solution {
-    int f(int i, int t, vector<int>& nums) {
-        if (i >= nums.size())
-            return 0;
-        if (dp[i][t] != -1)
-            return dp[i][t];
-        int p = 0, q = 0;
-        if (i == nums.size() - 1 && t == 0)
-            p = nums[i] + f(i + 2, t, nums);
-        else if (i != nums.size() - 1) {
-            p = nums[i] + f(i + 2, t | (i == 0), nums);
-        }
-        q = f(i + 1, t, nums);
-        return dp[i][t] = max(p, q);
-    }
-
 public:
-    int rob(vector<int>& nums) {
-        memset(dp, -1, sizeof dp);
-        return f(0, 0, nums);
+    int func(int indx, vector<int>& valueInHouse, vector<int>& dp) {
+        // if (indx == 0) {
+        //     return valueInHouse[indx];
+        // }
+        if (indx < 0) {
+            return 0;
+        }
+        if (dp[indx] != -1) {
+            return dp[indx];
+        }
+        int pick = valueInHouse[indx] + func(indx - 2, valueInHouse, dp);
+        int notPick = 0 + func(indx - 1, valueInHouse, dp);
+        return dp[indx] = max(pick, notPick);
+    }
+    int rob(vector<int>& valueInHouse) {
+
+        int n = valueInHouse.size();
+        if(n==1) return valueInHouse[0];
+        vector<int> dp(n, -1);
+        vector<int> temp1, temp2;
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                temp1.push_back(valueInHouse[i]);
+            }
+            if (i != n - 1) {
+                temp2.push_back(valueInHouse[i]);
+            }
+        }
+
+        int x = func(temp1.size() - 1, temp1, dp);
+        dp.assign(n, -1);
+        int y = func(temp2.size() - 1, temp2, dp);
+        // cout << temp2[0] << endl;
+        return max(x, y);
     }
 };
