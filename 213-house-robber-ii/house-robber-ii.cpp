013b38ee561c1e0af8aee049c96856910f42bdcb
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-05-18 18:56:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (26.75%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int func(int indx, vector<int>& valueInHouse, vector<int>& dp) {
-        // if (indx == 0) {
-        //     return valueInHouse[indx];
-        // }
+        if (indx == 0) {
+            return valueInHouse[indx];
+        }
         if (indx < 0) {
             return 0;
         }
@@ -17,7 +17,8 @@ public:
     int rob(vector<int>& valueInHouse) {
 
         int n = valueInHouse.size();
-        if(n==1) return valueInHouse[0];
+        if (n == 1)
+            return valueInHouse[0];
         vector<int> dp(n, -1);
         vector<int> temp1, temp2;
         for (int i = 0; i < n; i++) {
