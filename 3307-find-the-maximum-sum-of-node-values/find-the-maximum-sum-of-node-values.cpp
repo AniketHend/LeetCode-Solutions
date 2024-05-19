e372c02eaab8c36c983e259a69565c197c60d861
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-05-19 07:09:11 +0530

    Time: 359 ms (9.11%) | Memory: 199.7 MB (5.11%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-static const int _ = []() {ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); return 0;}();
+static const int _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
 
 #ifdef ONLINE_JUDGE
 #include "./debug.h"
@@ -27,11 +32,24 @@ typedef long double ld;
 const int mod = 1000000007; // 1000000007 998244353
 const ll inf = 1e18;
 
-template <typename Func> class FUN_res {Func func_; public: template <typename T>explicit FUN_res(T&& func) : func_(std::forward<T>(func)) {} template <typename... Args> decltype(auto) operator()(Args&&... args) {return func_(std::ref(*this), std::forward<Args>(args)...);}}; template <typename Func> decltype(auto) FUN(Func&& func) {return FUN_res<std::decay_t<Func>>(std::forward<Func>(func));}
+template <typename Func> class FUN_res {
+    Func func_;
+
+public:
+    template <typename T>
+    explicit FUN_res(T&& func) : func_(std::forward<T>(func)) {}
+    template <typename... Args> decltype(auto) operator()(Args&&... args) {
+        return func_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+template <typename Func> decltype(auto) FUN(Func&& func) {
+    return FUN_res<std::decay_t<Func>>(std::forward<Func>(func));
+}
 
 class Solution {
 public:
-    long long maximumValueSum(vector<int> nums, int k, vector<vector<int>> edges) {
+    long long maximumValueSum(vector<int> nums, int k,
+                              vector<vector<int>> edges) {
         ll n = sz(nums);
         vector<vector<ll>> a(n);
         for (int i = 1; i < n; i++) {
@@ -41,23 +59,26 @@ public:
             a[v].push_back(u);
         }
         vector dp(n, vi(2));
-        FUN([&](const auto & dfs, ll u, ll p)->void{
+        FUN([&](const auto& dfs, ll u, ll p) -> void {
             dp[u][0] = nums[u];
             dp[u][1] = nums[u] ^ k;
             for (auto v : a[u]) {
-                if (v == p) continue;
+                if (v == p)
+                    continue;
                 dfs(v, u);
                 vi ndp(2);
                 ndp[0] = max(dp[v][0] + dp[u][0], dp[v][1] + dp[u][1]);
                 ndp[1] = max(dp[v][1] + dp[u][0], dp[v][0] + dp[u][1]);
-                dp[u]= ndp;
+                dp[u] = ndp;
             }
-        })(0, 0);
+        })
+        (0, 0);
         return dp[0][0];
     }
 };
 void solve() {
-    dbg(Solution().maximumValueSum({7, 7, 7, 7, 7, 7}, 3, {{0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}}));
+    dbg(Solution().maximumValueSum({7, 7, 7, 7, 7, 7}, 3,
+                                   {{0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}}));
 }
 
 #ifdef LOCAL
