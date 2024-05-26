042ef82815c24bc7a3062e2272895874cc25d994
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-05-26 10:45:32 +0530

    Time: 2203 ms (7.69%) | Memory: 201.4 MB (7.69%) - LeetSync

--- 3444-find-the-number-of-good-pairs-ii/find-the-number-of-good-pairs-ii.cpp
+++ 3444-find-the-number-of-good-pairs-ii/find-the-number-of-good-pairs-ii.cpp
@@ -1,31 +1,29 @@
+#define ll long long 
+int spf[1000001];
+
 class Solution {
-public: 
-    
-   long long funcy(int num , unordered_map<long long,long long>&mp ){
-       long long x=0;
-       for(int j = 1; (long long)j * j <= num; ++j) {
-                if(num % j == 0) {
-                    if(mp.find(j) != mp.end()) {
-                        x += mp[j];
-                    }
-                    if(j != num / j && mp.find(num / j) != mp.end()) {
-                        x+= mp[num / j];
+public:
+    long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        map<ll,ll>mp;
+        for(int i = 0;i<nums1.size();i++){
+            for(ll div = 1;div*div<=nums1[i];div++){
+                if(nums1[i]%div==0){
+                    int first = nums1[i]/div;
+                    int second = div;
+                    if(first == second){
+                        mp[div]++;
+                    }else{
+                        mp[first]++;mp[second]++;
                     }
                 }
-            } 
-       
-       return x ; 
-   }
-    long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-            unordered_map<long long, long long> mp; 
-        for(int num : nums2) {
-            mp[(long long)num * k]++;
+            }
         }
-
-        long long result = 0;
-        for(int i=0;i<nums1.size();i++) {
-            result+=funcy(nums1[i],mp);
+        ll cnt = 0;
+        for(int i =0;i<nums2.size();i++){
+            ll val = k*1ll*nums2[i];
+            cnt+= mp[val];
         }
-        return result;
+        return cnt;
+        
     }
 };
