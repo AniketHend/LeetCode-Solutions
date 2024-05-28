a641bf8c6f903f625b7b42dd741da95ae4581054
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-05-28 11:22:58 +0530

    Time: 2817 ms (5.00%) | Memory: 497.5 MB (5.11%) - LeetSync

---
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -0,0 +1,82 @@
+class Solution {
+public:
+    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    bool surround(int i, int j, vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for (auto dir : directions) {
+            int neighbour_i = i + dir[0];
+            int neighbour_j = j + dir[1];
+
+            if (neighbour_i < 0 || neighbour_j >= m || neighbour_j < 0 ||
+                neighbour_i >= n)
+                return false;
+
+            if (grid[neighbour_i][neighbour_j] == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    void dfs(int i, int j, vector<vector<int>>& grid,
+             vector<vector<int>>& path_visi, int& profit, int& maxi_profit) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        path_visi[i][j] = 1;
+        profit += grid[i][j];
+
+        maxi_profit = max(profit, maxi_profit);
+
+        for (auto dir : directions) {
+            int neighbour_i = i + dir[0];
+            int neighbour_j = j + dir[1];
+
+            if (neighbour_i < 0 || neighbour_j < 0 || neighbour_i >= n ||
+                neighbour_j >= m)
+                continue;
+
+            if (grid[neighbour_i][neighbour_j] == 0 ||
+                path_visi[neighbour_i][neighbour_j] == 1)
+                continue;
+
+            dfs(neighbour_i, neighbour_j, grid, path_visi, profit, maxi_profit);
+        }
+
+        path_visi[i][j] = 0;
+        profit -= grid[i][j];
+    }
+
+    int getMaximumGold(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int maxi = 0;
+
+        vector<vector<int>> path_visi(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] != 0) {
+
+                    if (surround(i, j, grid))
+                        continue;
+
+                    int profit = 0;
+                    int maxi_profit = 0;
+
+                    dfs(i, j, grid, path_visi, profit, maxi_profit);
+                    maxi = max(maxi_profit, maxi);
+                }
+            }
+        }
+
+        return maxi;
+    }
+};
