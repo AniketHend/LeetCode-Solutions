5a83054e33e2a4b771969a4a120c6150a442d83d
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-05-28 12:44:34 +0530

    Time: 130 ms (64.51%) | Memory: 9.2 MB (46.15%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -1,75 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-    bool surround(int i, int j, vector<vector<int>>& grid) {
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        for (auto dir : directions) {
-            int neighbour_i = i + dir[0];
-            int neighbour_j = j + dir[1];
-
-            if (neighbour_i < 0 || neighbour_j >= m || neighbour_j < 0 ||
-                neighbour_i >= n)
-                return false;
-
-            if (grid[neighbour_i][neighbour_j] == 0)
-                return false;
-        }
-
-        return true;
-    }
-
-    void dfs(int i, int j, vector<vector<int>>& grid,
-             vector<vector<int>>& path_visi, int profit, int& maxi_profit) {
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        path_visi[i][j] = 1;
-        profit += grid[i][j];
-        maxi_profit = max(profit, maxi_profit);
-
-        for (auto dir : directions) {
-            int neighbour_i = i + dir[0];
-            int neighbour_j = j + dir[1];
-            if (neighbour_i < 0 || neighbour_j < 0 || neighbour_i >= n ||
-                neighbour_j >= m)
-                continue;
-            if (grid[neighbour_i][neighbour_j] == 0 ||
-                path_visi[neighbour_i][neighbour_j] == 1)
-                continue;
-            dfs(neighbour_i, neighbour_j, grid, path_visi, profit, maxi_profit);
-        }
-
-        path_visi[i][j] = 0;
-        profit -= grid[i][j];
-    }
-
-    int getMaximumGold(vector<vector<int>>& grid) {
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        int maxi = 0;
-
-        vector<vector<int>> path_visi(n, vector<int>(m, 0));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] != 0) {
-                    if (surround(i, j, grid))
-                        continue;
-                    int profit = 0;
-                    int maxi_profit = 0;
-                    dfs(i, j, grid, path_visi, profit, maxi_profit);
-                    maxi = max(maxi_profit, maxi);
-                }
+    vector<vector<int>> next = {{0,1},{0,-1},{1,0},{-1,0}};
+    int getMaximumGold(vector<vector<int>>& g) {
+        int res =0,n=g.size(),m=g[0].size();
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                res = max(res, backTrack(g,i,j,n,m));
             }
         }
+        return res;
+    }
 
-        return maxi;
+    int backTrack(vector<vector<int>>& g, int r, int c, int n, int m) {
+        if(!isValid(r, c, n, m) || g[r][c]==0) return 0;
+        int currVal = g[r][c];
+        g[r][c]=0;
+        int res = currVal;
+        int nextRes = 0;
+        for(int i=0;i<4;i++) {
+            int nextR = r + next[i][0];
+            int nextC = c + next[i][1];
+            nextRes = max(backTrack(g,nextR, nextC,n,m), nextRes);
+        }
+        g[r][c]=currVal;
+        return res + nextRes;
+    }
+
+    bool isValid(int r, int c, int n, int m) {
+        bool res = (r>=0 && c>=0 && r<n && c<m);
+        return res;
     }
 };
