428e67ac5fa7d78953ecbffe400ee89b3b1445ce
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-05-30 10:39:45 +0530

    Time: 134 ms (62.05%) | Memory: 10.2 MB (19.94%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -1,42 +1,92 @@
-static const int _ = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}();
-
 class Solution {
 public:
-    int mp,n,m;
-    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, { -1, 0}};
-    void dfs(int i, int j, vector<vector<int>>& grid, int profit) {
-        int x = grid[i][j];
-        profit += x;
-        grid[i][j] = 0;
-        mp = max(profit,  mp);
-        for (const auto &dir : directions) {
+    
+    vector<vector<int>> directions = {{0,1},{1,0},{0,-1},{-1,0}};
+    
+    bool surround(int i,int j,vector<vector<int>>& grid){
+        
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for(auto &dir:directions){
             int neighbour_i = i + dir[0];
             int neighbour_j = j + dir[1];
-            if (neighbour_i < 0 || neighbour_j < 0 || neighbour_i >= n || neighbour_j >= m)
-                continue;
-            if (grid[neighbour_i][neighbour_j] == 0)
-                continue;
-            dfs(neighbour_i, neighbour_j, grid, profit);
+            
+        if(neighbour_i < 0 || neighbour_j >= m || neighbour_j < 0 || neighbour_i >= n)
+           return false;
+
+          if(grid[neighbour_i][neighbour_j] == 0)
+           return false;      
+
         }
-        grid[i][j] = x;
+
+        return true;
+
     }
+
+
+    void dfs(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& path_visi,vector<vector<int>>& visi,int& profit,int& maxi_profit){
+        
+        int n = grid.size();
+        int m = grid[0].size();
+
+        path_visi[i][j] = 1;
+        profit += grid[i][j];
+
+        maxi_profit = max(profit,maxi_profit);
+
+        for(auto &dir:directions){
+            int neighbour_i = i + dir[0];
+            int neighbour_j = j + dir[1];
+
+            if( neighbour_i < 0 ||  neighbour_j < 0 || neighbour_i >= n || neighbour_j >= m)
+             continue;
+
+            if(grid[neighbour_i][neighbour_j] == 0 || path_visi[neighbour_i][neighbour_j] == 1)
+             continue; 
+    
+            
+            dfs(neighbour_i,neighbour_j,grid,path_visi,visi,profit,maxi_profit); 
+  
+        }
+
+        path_visi[i][j] = 0;
+        profit -= grid[i][j];
+
+    }
+
     int getMaximumGold(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
-        mp = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] != 0) {
-                    int profit = 0;
-                    dfs(i, j, grid, profit);
-                }
+        
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int maxi = 0;
+
+        vector<vector<int>> visi(n,vector<int> (m,0));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] != 0){   
+                 vector<vector<int>> path_visi(n,vector<int> (m,0));
+
+                  if(surround(i,j,grid))
+                   continue;    
+
+                 int profit = 0;
+                 int maxi_profit = 0;
+
+                 dfs(i,j,grid,path_visi,visi,profit,maxi_profit);
+
+                 visi[i][j] = 1;
+
+                 maxi = max(maxi_profit,maxi);
+
+                } 
             }
         }
-        return mp;
+
+        return maxi;
+        
+
     }
 };
