347c2363c6aa4523e5fc83f0e65ef2e0878093a7
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-02 18:32:34 +0530

    Time: 6 ms (34.84%) | Memory: 9.2 MB (29.26%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int dp[2001];
-    int mod = 1e9 + 7;
-    int fn(string& s, int i) {
-        if (i == s.length())
-            return 0;
-        if (dp[i] != -1)
-            return dp[i];
-        vector<int> vis(26, 0);
-        int ans = 0;
-        for (int j = i; j < s.length(); j++) {
-            if (vis[s[j] - 'a'])
-                continue;
-            vis[s[j] - 'a'] = 1;
-            ans = (ans + 1 + fn(s, j + 1)) % mod;
+    int distinctSubseqII(string str) {
+        int size=str.size();
+        vector<int> dp(size+1);
+        dp[0]=1;
+        
+        unordered_map<char,int> map;
+            
+        for(int i=0;i<size;i++){  
+            dp[i+1]=(dp[i]*2)%1000000007;
+            
+            //if the char already exist earlier
+            if(map.find(str[i])!=map.end()){
+                int repeatIndex=map[str[i]]-1;
+                dp[i+1]=(dp[i+1]-dp[repeatIndex]);
+                dp[i+1]%=1000000007;
+            }
+            map[str[i]]=i+1;
         }
-        return dp[i] = ans;
-    }
-    int distinctSubseqII(string s) {
-        memset(dp, -1, sizeof(dp));
-        return fn(s, 0);
+        dp[size]--;
+        if(dp[size]<0){
+            dp[size]+=1000000007;
+        }
+        return dp[size];
     }
 };
