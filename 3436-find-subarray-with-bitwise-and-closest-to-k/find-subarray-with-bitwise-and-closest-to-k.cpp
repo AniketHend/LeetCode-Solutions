01ce932a35ce7223077ab0316abac7b7858dfd8b
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-02 17:00:45 +0530

    Time: 503 ms (100.00%) | Memory: 265.3 MB (8.33%) - LeetSync

---
+++ 3436-find-subarray-with-bitwise-and-closest-to-k/find-subarray-with-bitwise-and-closest-to-k.cpp
@@ -0,0 +1,34 @@
+// solution
+class Solution {
+public:
+    int minimumDifference(vector<int>& A, int k) {
+        int N = A.size();
+        vector<vector<int>> bit(N + 1, vector<int>(30));
+        for (int i = 0; i < N; ++i) {
+            bit[i + 1] = bit[i];
+            for (int bt = 29; bt >= 0; --bt)
+                if (A[i] >> bt & 1)
+                    bit[i + 1][bt] += 1;
+        }
+
+        auto get = [&](int l, int r) {
+            int ans = 0;
+            for (int i = 0; i < 30; ++i)
+                if (bit[r][i] - bit[l][i] == r - l) {
+                    ans ^= 1 << i;
+                }
+            return ans;
+        };
+
+        int l = 0;
+        int ans = abs(A[0] - k);
+        for (int i = 0; i < N; ++i) {
+            while (l < i && get(l, i + 1) < k) {
+                ans = min(ans, abs(get(l, i + 1) - k));
+                ++l;
+            }
+            ans = min(ans, abs(get(l, i + 1) - k));
+        }
+        return ans;
+    }
+};
