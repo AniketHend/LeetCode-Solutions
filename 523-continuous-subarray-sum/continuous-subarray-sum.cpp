8083b1e761d52380b3bb68173657b7ad0fdefa10
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-08 11:38:34 +0530

    Time: 153 ms (95.58%) | Memory: 110.3 MB (99.11%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& v, int k) {
-        // sort(v.begin(), v.end());
-        if (v.size() < 2)
-            return false;
-        if(k==1) return true;
         long long maxi = 0;
         long long sum = 0;
         for (int i = 0; i < v.size(); i++) {
             sum += v[i];
         }
-        long long limit = sum / k + 1;
-        bool ok = false;
-        // for (auto& i : v) {
-        //     if (i == 0)
-        //         return true;
-        // }
+
         for (int i = 0; i < v.size() - 1; i++) {
             if (v[i] == v[i + 1] && v[i] == 0)
                 return true;
         }
-        for (long long i = k; i <= sum; i += k) {
+
+        if (k == 1 && v.size() > 1) {
+            return true;
+        }
+        if (v.size() < 2) {
+            return false;
+        }
+
+        long long limit = sum / k + 1;
+        bool ok = false;
+
+        for (int i = k; i <= sum; i += k) {
             long long l = 0;
-            long long tempsum = 0;
-            for (int r = 0; r < v.size(); r++) {
+            int tempsum = v[0];
+            for (int r = 1; r < v.size(); r++) {
                 tempsum += v[r];
                 while (tempsum > i) {
                     tempsum -= v[l];
                     l++;
                 }
-                if (r - l >= 1 && tempsum == i) {
-                    return true;
+                if ((tempsum == i && r - l >= 1) ||
+                    (tempsum == 0 && r - l >= 1)) {
+                    ok = true;
+                    break;
                 }
             }
-            if (v.size() - 1 - l >= 1 && tempsum == i) {
-                return true;
-            }
+            if(ok) break;
         }
         return ok;
     }
