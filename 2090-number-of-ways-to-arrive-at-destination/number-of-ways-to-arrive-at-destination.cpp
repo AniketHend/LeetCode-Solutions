3155b6e0a41cc1d145494bc98236ef1256647fc2
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-08 09:58:17 +0530

    Time: 59 ms (55.43%) | Memory: 39.4 MB (9.07%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,40 +1,40 @@
+
+#define ll long long
+const ll mod = 1e9 + 7;
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<long long, long long>>> adj(n);
-        for (int i = 0; i < roads.size(); i++) {
-            adj[roads[i][0]].push_back({roads[i][1], roads[i][2]});
-            adj[roads[i][1]].push_back({roads[i][0], roads[i][2]});
+        vector<pair<ll, ll>> g[n + 1];
+        for (auto it : roads) {
+            g[it[0]].push_back({it[1], it[2]});
+            g[it[1]].push_back({it[0], it[2]});
         }
-        priority_queue<pair<long long, long long>,
-                       vector<pair<long long, long long>>,
-                       greater<pair<long long, long long>>>
-            pq;
-        vector<long long> dist(n, LONG_MAX);
-        vector<long long> ways(n, 0);
-        pq.push({0, 0});
+        set<pair<ll, ll>> st;
+        st.insert({0, 0});
+        vector<ll> dist(n, 1e12);
         dist[0] = 0;
-        ways[0] = 1;
-        int mod = 1000000007;
-        while (!pq.empty()) {
-            long long node = pq.top().second;
-            long long distance = pq.top().first;
-            pq.pop();
-            if (distance > dist[node])
-                continue;
-            for (auto& it : adj[node]) {
-                long long adjnode = it.first;
-                long long edgewt = it.second;
-                if (distance + edgewt < dist[adjnode]) {
-                    dist[adjnode] = dist[node] + edgewt;
-                    ways[adjnode] = ways[node];
-                    pq.push({dist[adjnode], adjnode});
-                } else if (dist[node] + edgewt == dist[adjnode]) {
-                    ways[adjnode] += ways[node];
-                    ways[adjnode] %= mod;
+        vector<ll> nosWays(n, 0);
+        nosWays[0] = 1;
+        while (!st.empty()) {
+            pair<ll, ll> p = *st.begin();
+            st.erase(p);
+            ll dist_p = p.first;
+            ll node = p.second;
+            // if(dist_p > dist[node]) continue;
+            for (auto c : g[node]) {
+                ll to = c.first;
+                ll w = c.second;
+                if (dist_p + w < dist[to]) {
+                    st.erase({dist[to], to});
+                    nosWays[to] = nosWays[node];
+                    dist[to] = dist_p + w;
+                    st.insert({dist[to], to});
+                } else if (dist_p + w == dist[to]) {
+                    nosWays[to] = (nosWays[node] + nosWays[to]) % mod;
                 }
             }
         }
-        return (ways[n - 1]) % mod;
+        cout << dist[n - 1] << endl;
+        return (int)nosWays[n - 1];
     }
 };
