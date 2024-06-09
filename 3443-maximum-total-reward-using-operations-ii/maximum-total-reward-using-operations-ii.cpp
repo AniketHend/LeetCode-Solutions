3af60bdac7231090c11eedf56e47d4471736cca0
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-09 10:54:17 +0530

    Time: 682 ms (40.00%) | Memory: 51.9 MB (40.00%) - LeetSync

---
+++ 3443-maximum-total-reward-using-operations-ii/maximum-total-reward-using-operations-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxTotalReward(vector<int>& a) {
+        const int N = 100100;
+        bitset<N> dp;
+        dp[0] = 1;
+        bitset<N> ndp;
+
+        sort(a.begin(), a.end());
+        a.erase(unique(a.begin(), a.end()), a.end());
+        int p = 0;
+        for (auto& num : a) {
+            while (p < num)
+                ndp.set(p++);
+            dp |= ((dp & ndp) << num);
+        }
+        int y = N - 1;
+        while (!dp[y])
+            y--;
+        return y;
+    }
+};
