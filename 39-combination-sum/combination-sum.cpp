3bc7ba01a564769148d2d6497d25308738032893
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-09 16:54:41 +0530

    Time: 140 ms (5.02%) | Memory: 113.5 MB (5.00%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    void solve(vector<int> &candidates, int index, int sum, int target,
+    void solve(vector<int> candidates, int index, int sum, int target,
                vector<int> support, vector<vector<int>>& ans) {
         if (sum == target) {
             ans.push_back(support);
             return;
         }
-        if (sum > target) {
-            return;
-        }
         if (index >= candidates.size()) {
             return;
         }
+        if (sum > target) {
+            return;
+        }
+
         support.push_back(candidates[index]);
         solve(candidates, index, sum + candidates[index], target, support, ans);
         support.pop_back();
         solve(candidates, index + 1, sum, target, support, ans);
+        return;
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
