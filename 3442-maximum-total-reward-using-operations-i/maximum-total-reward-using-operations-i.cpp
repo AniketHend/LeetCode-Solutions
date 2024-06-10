6e4bc8e3cfa43b41537df01a7ffb5ec56e09a79f
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-10 18:42:38 +0530

    Time: 272 ms (100.00%) | Memory: 43.5 MB (60.00%) - LeetSync

--- 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
+++ 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    int maxTotalReward(vector<int>& A) {
-        sort(A.begin(), A.end());
-        A.erase(unique(A.begin(), A.end()), A.end());
-        int n = A.size();
-        int sum = 0;
+    int maxTotalReward(vector<int>& rewards) {
+        if (rewards.size() == 1)
+            return rewards[0];
+        int n = rewards.size();
+        vector<int> dp(4002 + 1, 0), ndp(4002, 0);
+        sort(rewards.begin(), rewards.end());
 
-        map<int, int> dp, ndp;
-        dp[0] = 0;
-        for (int i = 0; i < n; i++) {
-            ndp = dp;
-            for (auto& [x, y] : dp) {
-                if (x < A[i]) {
-                    ndp[A[i] + x]++;
-                    sum = max(sum, A[i] + x);
+        for (int r = 0; r < 4002; r++) {
+            if (rewards[n - 1] > r)
+                dp[r] = rewards[n - 1];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int rm = 0; rm < 4002; rm++) {
+                if (rewards[i] > rm) {
+                    if (rm + rewards[i] > 4002)
+                        ndp[rm] = max(dp[rm], rewards[i]);
+                    ndp[rm] = max(dp[rm], rewards[i] + dp[rm + rewards[i]]);
                 } else
-                    break;
+                    ndp[rm] = dp[rm];
             }
             dp = ndp;
         }
-        return sum;
+
+        return dp[0];
     }
 };
