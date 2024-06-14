d50210bb18c60f7c65bc19c89cc538def61f22b3
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-14 16:49:12 +0530

    Time: 3 ms (11.05%) | Memory: 44 MB (62.47%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.java
+++ 983-validate-stack-sequences/validate-stack-sequences.java
@@ -4,26 +4,24 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         for (int i = 0; i < pushed.length; i++) {
             st.push(pushed[i]);
-            while(j<popped.length && st.size()>0 && st.peek()==popped[j]) {
+            while (j < popped.length && st.size() > 0 && st.peek() == popped[j]) {
                 st.pop();
                 j++;
             }
             // if (pushed[i] == popped[j]) {
-            //     // i++;
-            //     j++;
+            // // i++;
+            // j++;
             // } else if (st.size() > 0 && popped[j] == st.peek()) {
-            //     st.pop();
-            //     j++;
+            // st.pop();
+            // j++;
             // } else {
-            //     // i++;
+            // // i++;
             // }
         }
         while (j < popped.length) {
             if (popped[j] != st.peek()) {
                 return false;
             }
-            if (st.empty())
-                return false;
             st.pop();
             j++;
         }
