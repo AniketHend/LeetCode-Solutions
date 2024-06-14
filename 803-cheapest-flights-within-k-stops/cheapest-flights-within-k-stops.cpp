522302c290f61de9fcc154ecd1deafc8085f0d49
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-14 20:21:38 +0530

    Time: 17 ms (55.29%) | Memory: 17.1 MB (50.25%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,38 +1,36 @@
-// dp[i][j] denotes the minimum cost to reach i from src using less than or
-// equal to j stpes
-
-// dp[i][j] = min(dp[i][j-1],X) X = dp[node][j-1] + w(node,i);
-
-// dp[i][j] = 1e9
-// dp[scr][0]=0;
-static const int _ = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}();
-#define ll long long
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
-                          int k) {
-        vector<ll> dist(n, 1e12);
-        dist[src] = 0;
+                          int K) {
+        vector<vector<pair<int, int>>> g(n + 1);
+        for (auto i : flights) {
+            g[i[0]].push_back({i[1], i[2]});
+        }
 
-        vector<ll> next(n, 1e12);
-        for (int i = 1; i <= k + 1; i++) {
-            next = dist;
-            for (auto it : flights) {
-                ll from = it[0];
-                ll to = it[1];
-                ll w = it[2];
-                next[to] = min(next[to], dist[from] + w);
+        queue<pair<int, pair<int, int>>> q;
+        vector<int> dis(n + 1, INT_MAX);
+        dis[src] = 0;
+        q.push({0, {src, 0}});
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            int stop = it.first;
+            if (stop > K)
+                continue;
+            int node = it.second.first;
+            int distance = it.second.second;
+            for (auto k : g[node]) {
+                int newn = k.first;
+                int costnew = k.second;
+                if (dis[newn] > (distance + costnew) and stop <= K) {
+                    dis[newn] = distance + costnew;
+                    q.push({stop + 1, {newn, distance + costnew}});
+                }
             }
-            dist = next;
         }
-        if (dist[dst] == 1e12) {
+        if (dis[dst] == INT_MAX)
             return -1;
-        }
-        return dist[dst];
+        else
+            return dis[dst];
     }
 };
