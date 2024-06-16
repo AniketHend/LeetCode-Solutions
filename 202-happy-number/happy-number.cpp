2e6acbeed1f2456c278ff883ce7bcbd4a088a967
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-16 15:50:40 +0530

    Time: 2 ms (49.41%) | Memory: 8.8 MB (5.90%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -4,11 +4,7 @@ public:
         map<int, int> mp;
         mp[n]++;
         int x = n;
-        while (x) {
-            if (x == 1)
-                break;
-            if (mp[x] > 1)
-                break;
+        while (x != 1 && mp[x]<2) {
             int s = 0;
             while (x > 0) {
                 s = s + ((x % 10) * (x % 10));
