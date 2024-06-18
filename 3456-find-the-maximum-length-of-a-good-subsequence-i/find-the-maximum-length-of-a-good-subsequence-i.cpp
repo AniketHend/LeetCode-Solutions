aa3023554b81e9350a4cefadcccf91d2b498beea
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-18 09:05:39 +0530

    Time: 1943 ms (16.81%) | Memory: 621.7 MB (5.36%) - LeetSync

--- 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.cpp
+++ 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.cpp
@@ -1,52 +1,28 @@
-//dp state
-//dp[i][j][cnt] denotes the maximum possible length of good subsequence from 0 to i th index such that ith index is considered into subsequce(1) or not(0)
-//such tht cnt is nos of indices satisfying conidtion
-
-//transition equations
-//dp[i][1][cnt] = 1+ dp[j][1][cnt] if v[i]==v[j], including prev one and curr
-// else dp[i][1][cnt] = 1 + dp[j][0][cnt], including curr one
-// if(cnt<=k)dp[i][1][cnt] = 1+ dp[i-1][1][cnt-1];// including both
-
-//dp[i][0][cnt] = max(dp[j][0][cnt],dp[j][1][cnt]);
-
-//base case
-//dp[0][0][0] = 0;
-//dp[0][1][0] = 1;
-
-//answer
-//max(dp[n-1][1/0][cnt])
-
 class Solution {
 public:
-    int maximumLength(vector<int>& nums, int k) {
+    int maximumLength(vector<int>& nums, int op) {
+        // map<tuple<int,int,int>, int> dp;
         int n = nums.size();
-       vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
-       for(int cnt=0;cnt<=k;cnt++)dp[0][1][cnt] = 1;
-       map<int,int>prev;
-       prev[nums[0]] = 0;
-       for(int i = 1;i<n;i++){
-            
-                    for(int cnt = 0;cnt<=k;cnt++){
-                        //exclude current one
-                        dp[i][0][cnt] = max({dp[i-1][0][cnt],dp[i][0][cnt],dp[i-1][1][cnt]});
-                        dp[i][1][cnt]=1;
-                        if(prev.find(nums[i])!=prev.end()){
-                            dp[i][1][cnt] = max(dp[i][1][cnt], 1+ dp[prev[nums[i]]][1][cnt]);
-                        }
-                        if(cnt >0){
-                            dp[i][1][cnt] = max(dp[i][1][cnt],1 + dp[i-1][0][cnt-1]);
-                        }
-                        if((nums[i-1]!=nums[i]) && cnt>0)dp[i][1][cnt] = max(dp[i][1][cnt],1+dp[i-1][1][cnt-1]);
-                        
-                    }
-            prev[nums[i]]=i;
-       }
-       int ans =0 ;
-       for(int inc = 0;inc<2;inc++){
-            for(int cnt = 0;cnt<=k;cnt++){
-                ans = max(ans,dp[n-1][inc][cnt]);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(op+1,-1)));
+        function<int(int, int, int)> fn = [&](int i, int j, int k) {
+            if (i == n) return 0;
+            if (dp[i][j+1][k]!=-1) return dp[i][j+1][k];
+            int ans = 0;
+            if (j == -1){
+                ans = max(ans, max(1 + fn(i+1, i, k), fn(i+1, j, k)));
             }
-       }
-       return ans;
+            if (k >= 0 && j != -1){
+                if (nums[i] == nums[j]) {
+                    ans = max(ans, 1+fn(i+1, i, k));
+                } else {
+                    ans = max(ans, fn(i+1, j, k));
+                }
+            }
+            if (k > 0 && j != -1){
+                ans = max(ans, 1+fn(i+1,i,k-1));
+            }
+            return dp[i][j+1][k] = ans;
+        };
+        return fn(0, -1, op);
     }
 };
