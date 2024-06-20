c2056ce02b661092c99a47a032b2fc327811f58f
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-20 12:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (80.96%) - LeetSync

--- 910-nth-magical-number/nth-magical-number.cpp
+++ 910-nth-magical-number/nth-magical-number.cpp
@@ -1,12 +1,4 @@
 const int MOD = 1e9 + 7;
-long long gcd(long long a, long long b) {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
-}
-
-long long lcm(long long a, long long b) { return (a / gcd(a, b)) * b; }
-
 class Solution {
 public:
     bool isPossible(long long mid, long long n, int a, int b) {
