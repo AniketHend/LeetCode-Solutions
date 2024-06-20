36b171786c76687c32f94cef5592f8ba5d213099
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-20 12:51:44 +0530

    Time: 3 ms (22.17%) | Memory: 7.1 MB (43.13%) - LeetSync

--- 910-nth-magical-number/nth-magical-number.cpp
+++ 910-nth-magical-number/nth-magical-number.cpp
@@ -1,36 +1,36 @@
+const int MOD = 1e9 + 7;
+long long gcd(long long a, long long b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+long long lcm(long long a, long long b) { return (a / gcd(a, b)) * b; }
+
 class Solution {
-private:
-    long long gcd(long long a, long long b) {
-        if (b == 0)
-            return a;
-        return gcd(b, a % b);
-    }
-
-    long long lcm(long long a, long long b) { return (a / gcd(a, b)) * b; }
-
-    long long checker(long long mid, int a, int b) {
-        long long count = 0;
-        count += mid / a;
-        count += mid / b;
-        count -= mid / (lcm(a, b));
-        return count;
-    }
-
 public:
-    int nthMagicalNumber(int n, int a, int b) {
-        long long low = 1;
-        long long high = 1e18;
-        long long ans;
+    bool isPossible(long long mid, long long n, int a, int b) {
+        long long x = mid / a;
+        long long y = mid / b;
+        long long z = mid / lcm(a, b);
+        return x + y - z >= n;
+    }
 
+    int nthMagicalNumber(int n, int a, int b) {
+        long long low = min(a, b);
+        long long high = 1e18;
+
+        long long ans = -1;
         while (low <= high) {
             long long mid = low + (high - low) / 2;
-            if (checker(mid, a, b) >= n) {
+            if (isPossible(mid, n, a, b)) {
                 ans = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return ans % ((long long)1e9 + 7);
+
+        return ans % MOD;
     }
 };
