4db286cff06e2d0b4d77dee0048bbe83177021fa
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-21 15:30:11 +0530

    Time: 4 ms (38.62%) | Memory: 8.7 MB (22.13%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,3 +1,10 @@
+static const int _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& a) {
