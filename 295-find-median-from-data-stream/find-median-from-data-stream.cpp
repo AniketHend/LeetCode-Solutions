bfda6b6a3f53efb7bbb7e67a926fdee0872451f3
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-25 18:56:10 +0530

    Time: 331 ms (15.09%) | Memory: 145.3 MB (6.09%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -5,7 +5,7 @@ const static auto fast = []
 { 
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    //std::cout.tie(nullptr);
+    std::cout.tie(nullptr);
     return 0;
 }();
 
