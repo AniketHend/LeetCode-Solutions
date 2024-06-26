993f60bef1b1d754b377ee30ad2581fd5e7662ae
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-26 11:41:07 +0530

    Time: 5 ms (33.86%) | Memory: 9.3 MB (52.10%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -3,9 +3,22 @@ public:
     vector<vector<int>> permute(vector<int>& s) {
         vector<vector<int>> sa;
         sort(s.begin(), s.end());
-        do {
-            sa.push_back(s);
-        } while (next_permutation(s.begin(), s.end()));
+        vector<int> v;
+        int n = s.size();
+        function<void(int)> f = [&](int msk) {
+            if (__builtin_popcount(msk) == s.size()) {
+                sa.push_back(v);
+                return;
+            }
+            for (int i = 0; i < n; i++) {
+                if (!(msk & (1 << i))) {
+                    v.push_back(s[i]);
+                    f(msk | (1 << i));
+                    v.pop_back();
+                }
+            }
+        };
+        f(0);
         return sa;
     }
 };
