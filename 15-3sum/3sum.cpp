e820c231d749c5cc605e3c24626a203086525f3a
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-06-27 10:04:52 +0530

    Time: 51 ms (93.99%) | Memory: 27.2 MB (79.42%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,27 +6,49 @@ static const int _ = []() {
 }();
 
 class Solution {
-public:
-    vector<vector<int>> ans;
-    vector<vector<int>> threeSum(vector<int>& a) {
-        int n = a.size();
-        vector<vector<int>> ans;
-        sort(a.begin(), a.end());
-        for (int i = 0; i < a.size() - 2; i++) {
-            if (i > 0 && a[i] == a[i-1]) continue;
-            int j = i + 1, k = a.size() - 1;
-            while (j < k) {
-                int csum = a[i] + a[j] + a[k];
-                if (csum == 0)
-                    ans.push_back({a[i], a[j], a[k]});
-                if (csum <= 0)
-                    j++;
-                else
-                    k--;
+    void nSum(const vector<int>& nums, long n, long target, int l, int r,
+              vector<int>& path, vector<vector<int>>& ans) {
+        if (r - l + 1 < n || target < nums[l] * n || target > nums[r] * n)
+            return;
+        if (n == 2) {
+            // Similar to the sub procedure in 15. 3Sum
+            while (l < r) {
+                const int sum = nums[l] + nums[r];
+                if (sum == target) {
+                    path.push_back(nums[l]);
+                    path.push_back(nums[r]);
+                    ans.push_back(path);
+                    path.pop_back();
+                    path.pop_back();
+                    ++l;
+                    --r;
+                    while (l < r && nums[l] == nums[l - 1])
+                        ++l;
+                    while (l < r && nums[r] == nums[r + 1])
+                        --r;
+                } else if (sum < target) {
+                    ++l;
+                } else {
+                    --r;
+                }
             }
+            return;
         }
-        sort(ans.begin(), ans.end());
-        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+
+        for (int i = l; i <= r; ++i) {
+            if (i > l && nums[i] == nums[i - 1])
+                continue;
+            path.push_back(nums[i]);
+            nSum(nums, n - 1, target - nums[i], i + 1, r, path, ans);
+            path.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> path;
+        ranges::sort(nums);
+        nSum(nums, 3, 0, 0, nums.size() - 1, path, ans);
         return ans;
     }
 };
