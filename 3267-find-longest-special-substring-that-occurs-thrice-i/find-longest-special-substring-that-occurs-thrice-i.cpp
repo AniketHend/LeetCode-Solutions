eebc5a37c64adfc92486fdc4c9d9cdfd661a72bb
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-07-02 09:10:53 +0530

    Time: 3 ms (98.45%) | Memory: 11 MB (74.94%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool check(string& s, int len) {
+        vector<int> v(26, 0);
+        vector<int> cur(26, 0);
+        int n = s.size();
+        for (int i = 0; i < len - 1; i++)
+            cur[s[i] - 'a']++;
+        for (int i = len - 1; i < n; i++) {
+            cur[s[i] - 'a']++;
+            if (i - len >= 0)
+                cur[s[i - len] - 'a']--;
+            if (cur[s[i] - 'a'] == len) {
+                v[s[i] - 'a'] += 1;
+            }
+            if (v[s[i] - 'a'] >= 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+    int maximumLength(string s) {
+        int n = s.size();
+        int l = 1, h = n;
+        int ans = -1;
+        cout << check(s, 2) << endl;
+        while (l <= h) {
+            int mid = (l + h) / 2;
+            if (check(s, mid)) {
+                ans = mid;
+                l = mid + 1;
+            } else
+                h = mid - 1;
+        }
+        return ans;
+    }
+};
