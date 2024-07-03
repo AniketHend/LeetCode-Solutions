7b7ff5163bc9369cfdd118e21448d1c5c8890835
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-07-03 10:01:44 +0530

    Time: 144 ms (58.25%) | Memory: 29.7 MB (52.01%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     int dp[201][20001];
-    // bool rec(int i , int target , vector<int>& nums)
-    // {
-    //     if(target==0)
-    //     {
-    //         return true;
-    //     }
-    //     if(i==0)
-    //     {
-    //         return nums[i]==target;
-    //     }
-    //     if(dp[i][target]!=-1)
-    //     {
-    //         return dp[i][target];
-    //     }
-    //     bool ans=false;
-    //     if(target>=nums[i])
-    //     {
-    //         ans = ans|rec(i-1,target-nums[i],nums);
-    //     }
-    //     ans |= rec(i-1,target,nums);
-    //     return dp[i][target]=ans;
-    // }
     bool canPartition(vector<int>& arr) {
         int n = arr.size();
         int sum = accumulate(arr.begin(), arr.end(), 0);
@@ -30,16 +8,16 @@ public:
         if (sum % 2 != 0) {
             return false;
         }
-        dp[n][0] = 1;
+        dp[0][0] = 1;
 
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = 1; i <= n; i++) {
             for (int target = sum / 2; target >= 1; target--) {
-                if (target >= arr[i]) {
-                    dp[i][target] |= dp[i + 1][target - arr[i]];
+                if (target >= arr[i - 1]) {
+                    dp[i][target] |= dp[i - 1][target - arr[i - 1]];
                 }
-                dp[i][target] |= dp[i + 1][target];
+                dp[i][target] |= dp[i - 1][target];
             }
         }
-        return dp[0][sum / 2];
+        return dp[n][sum / 2];
     }
 };
