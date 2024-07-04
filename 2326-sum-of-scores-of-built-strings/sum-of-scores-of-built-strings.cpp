02ff7b830c9e5f56913139ca70a60ca053315305
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-07-04 09:25:16 +0530

    Time: 534 ms (12.29%) | Memory: 69.8 MB (7.26%) - LeetSync

---
+++ 2326-sum-of-scores-of-built-strings/sum-of-scores-of-built-strings.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+#define ll long long int
+    static const ll M = 1e9 + 7;
+    static const ll BASE1 = 5689, BASE2 = 8861;
+
+    struct Hash {
+        vector<pair<ll, ll>> hashes, po;
+        Hash(string s) {
+            hashes.assign(s.size() + 1, {1, 1});
+            po.assign(s.size() + 1, {1, 1});
+            for (ll i = 0; i < (ll)(s.size()); i++) {
+                hashes[i + 1] = {
+                    (hashes[i].first * BASE1 + ((s[i] - 'a') + 1)) % M,
+                    (hashes[i].second * BASE2 + ((s[i] - 'a') + 1)) % M};
+                po[i + 1] = {(po[i].first * BASE1) % M,
+                             (po[i].second * BASE2) % M};
+            }
+        }
+
+        pair<ll, ll> g(ll l, ll r) {
+            l++, r++;
+            ll hash1 = (hashes[r].first -
+                        (hashes[l - 1].first * po[r - l + 1].first) % M + M) %
+                       M;
+            ll hash2 = (hashes[r].second -
+                        (hashes[l - 1].second * po[r - l + 1].second) % M + M) %
+                       M;
+            return {hash1, hash2};
+        }
+    };
+
+    long long sumScores(string s) {
+        struct Hash h = Hash(s);
+        ll n = s.size();
+        ll ans = 0;
+
+        for (ll i = n - 1; i >= 0; i--) {
+            ll lcp = 0;
+            ll left = i;
+            ll right = n - 1;
+            while (left <= right) {
+                ll mid = left + (right - left) / 2;
+                ll len = mid - i;
+                if (h.g(0, len) == h.g(i, mid)) {
+                    lcp = len + 1;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            ans += lcp;
+        }
+        return ans;
+    }
+};
