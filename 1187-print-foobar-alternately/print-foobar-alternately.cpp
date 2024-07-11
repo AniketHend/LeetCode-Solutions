1aeef854c0416f8ae24d83021a2925fd531d4463
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-07-11 11:39:22 +0530

    Time: 7 ms (67.20%) | Memory: 10 MB (75.97%) - LeetSync

--- 1187-print-foobar-alternately/print-foobar-alternately.cpp
+++ 1187-print-foobar-alternately/print-foobar-alternately.cpp
@@ -1,34 +1,43 @@
+
+
 class FooBar {
 private:
     int n;
-    atomic<bool> y;
+    std::condition_variable turn;
+    std::mutex m;
+    std::atomic<bool> isFooTurn;
 
 public:
     FooBar(int n) {
         this->n = n;
-        y = false;
+        isFooTurn.store(true); // Start with foo's turn
     }
 
-    void foo(function<void()> printFoo) {
-
+    void foo(std::function<void()> printFoo) {
+        std::unique_lock<std::mutex> lock(m);
         for (int i = 0; i < n; i++) {
-            while (y) {
-                this_thread::yield();
-            }
-            // printFoo() outputs "foo". Do not change or remove this line.
+            // Wait until it's foo's turn
+            turn.wait(lock, [this]() { return isFooTurn.load(); });
+            // Print "foo"
             printFoo();
-            y = !y;
+            // Change the turn to bar
+            isFooTurn.store(false);
+            // Notify one waiting thread (bar) to proceed
+            turn.notify_one();
         }
     }
 
-    void bar(function<void()> printBar) {
+    void bar(std::function<void()> printBar) {
+        std::unique_lock<std::mutex> lock(m);
         for (int i = 0; i < n; i++) {
-            while (!y) {
-                this_thread::yield();
-            }
-            // printBar() outputs "bar". Do not change or remove this line.
+            // Wait until it's bar's turn
+            turn.wait(lock, [this]() { return !isFooTurn.load(); });
+            // Print "bar"
             printBar();
-            y = !y;
+            // Change the turn to foo
+            isFooTurn.store(true);
+            // Notify one waiting thread (foo) to proceed
+            turn.notify_one();
         }
     }
 };
