3a1b232b048f75d9ec7cb36f025b8a97bf154e8f
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-07-16 17:52:08 +0530

    Time: 444 ms (64.80%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,2 @@
-# Write your MySQL query statement below
-select id from 
-(select w1.id,w1.temperature t1,w2.temperature t2 from Weather w1 cross join Weather w2
-where DATEDIFF(w1.recordDate,w2.recordDate) = 1) as P
-where P.t1 > P.t2;
+select w1.id from Weather w1 cross join Weather w2
+where DATEDIFF(w1.recordDate,w2.recordDate) = 1 and w1.temperature > w2.temperature
