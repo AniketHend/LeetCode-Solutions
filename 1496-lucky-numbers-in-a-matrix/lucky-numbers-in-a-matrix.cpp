ace2bf1abbbcd5fa39209b66f462ca5a238fcff5
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-07-19 13:54:17 +0530

    Time: 16 ms (56.65%) | Memory: 14.3 MB (27.66%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& a) {
+        int n=a.size(),m=a[0].size();
+        vector<int> r(n),c(m);
+        for(int i=0;i<n;i++) {
+            int mc=1e6,cnt=0;
+            for(int j=0;j<m;j++) {
+                mc=min(mc,a[i][j]);
+            }
+            r[i]=mc;
+        }
+        for(int i=0;i<m;i++) {
+            int mc=0,cnt=0;
+            for(int j=0;j<n;j++) {
+                mc=max(mc,a[j][i]);
+            }
+            c[i]=mc;
+        }
+        vector<int> sa;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(a[i][j]==r[i] && a[i][j]==c[j]) {
+                    sa.push_back(a[i][j]);
+                }
+            }
+        }
+        return sa;
+    }
+};
