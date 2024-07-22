38c6b8dd2247a4c848f7f92ee8b44e448aba1cd2
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-07-22 09:11:03 +0530

    Time: 7 ms (89.48%) | Memory: 21 MB (72.77%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.cpp
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -1,21 +1,19 @@
 class Solution {
-    unordered_map<TreeNode*, int> dp;
+    array<int, 2> robb(TreeNode* root) {
+        if (!root)
+            return {0, 0};
+        auto lft = robb(root->left);
+        auto rht = robb(root->right);
+        array<int, 2> c = {0, 0};
+        c[0] = root->val + lft[1] + rht[1];
+        c[1] = max(rht[0], rht[1]) + max(lft[0], lft[1]);
+        // cout << root->val << " " << c[0] << " " << c[1] << endl;
+        return c;
+    }
+
 public:
     int rob(TreeNode* root) {
-        if (!root)
-            return 0;
-        if (dp.count(root))
-            return dp[root];
-        int pick = root->val;
-        int x = 0;
-        if (root->left) {
-            x = rob(root->left->left) + rob(root->left->right);
-        }
-        if (root->right) {
-            x += rob(root->right->left) + rob(root->right->right);
-        }
-        pick += x;
-        int notpick = rob(root->left) + rob(root->right);
-        return dp[root] = max(pick, notpick);
+        array<int, 2> c = robb(root);
+        return max(c[0], c[1]);
     }
 };
