12267b6088fbba2efaad28abf1d81d6e803fc50f
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-07-24 10:28:45 +0530

    Time: 436 ms (32.47%) | Memory: 85.3 MB (87.88%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -1,3 +1,5 @@
+static const int _ = []() {ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); return 0;}();
+
 class Solution {
 public:
     vector<int> sortJumbled(vector<int>& M, vector<int>& nums) {
