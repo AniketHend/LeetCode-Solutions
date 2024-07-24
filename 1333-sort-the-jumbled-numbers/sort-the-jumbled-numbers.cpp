1d9da53652037927aae9a0ff9db0996d3d406c92
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-07-24 10:29:53 +0530

    Time: 420 ms (35.07%) | Memory: 76.6 MB (96.10%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -3,13 +3,10 @@ static const int _ = []() {ios::sync_with_stdio(false); cin.tie(nullptr); cout.t
 class Solution {
 public:
     vector<int> sortJumbled(vector<int>& M, vector<int>& nums) {
-        vector<pair<int, int>> vp;
-        for (int i = 0; i < nums.size(); ++i)
-            vp.push_back({nums[i], i});
-        sort(vp.begin(), vp.end(), [&](const auto& a, const auto& b) {
+        sort(nums.begin(), nums.end(), [&](const auto& a, const auto& b) {
             int num1 = 0, num2 = 0;
-            int x = a.first;
-            int y = b.first;
+            int x = a;
+            int y = b;
             int yp = 1;
             if(x == 0) num1 = M[0];
             while (x) {
@@ -20,13 +17,8 @@ public:
             while (y) {
                 num2 += M[y % 10] * yp, y /= 10, yp *= 10;
             }
-            if (num1 == num2)
-                return a.second < b.second;
             return num1 < num2;
         });
-        nums.clear();
-        for (auto it : vp)
-            nums.push_back(it.first);
         return nums;
     }
 };
