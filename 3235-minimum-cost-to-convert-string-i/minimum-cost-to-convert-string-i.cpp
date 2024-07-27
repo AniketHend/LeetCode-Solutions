112e99b5dac7e606cf579125785159626726f22f
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-07-27 09:23:51 +0530

    Time: 156 ms (80.97%) | Memory: 111.6 MB (32.92%) - LeetSync

---
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,58 @@
+#define ll long long
+#define INF 1e18
+#define ff first
+#define ss second
+
+class Solution {
+
+    void FloydWarshall(int n, vector<vector<pair<int, ll>>> adj,
+                       vector<vector<ll>>& dist) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = INF;
+            }
+            dist[i][i] = 0;
+        }
+        for (int i = 0; i < n; i++) {
+            for (auto j : adj[i]) {
+                dist[i][j.ff] = min(dist[i][j.ff], j.ss);
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] < INF &&
+                        dist[k][j] <
+                            INF) // for updating only when there is a path
+                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+
+public:
+    long long minimumCost(string S, string T, vector<char>& o, vector<char>& c,
+                          vector<int>& cost) {
+        vector<vector<pair<int, ll>>> adj(26);
+
+        for (int i = 0; i < o.size(); i++) {
+            int x = o[i] - 'a';
+            int y = c[i] - 'a';
+            adj[x].push_back({y, cost[i] * 1ll});
+        }
+        vector<vector<ll>> dis(26, vector<ll>(26));
+        FloydWarshall(26, adj, dis);
+
+        long long res = 0ll;
+        for (int i = 0; i < S.size(); i++) {
+            int x = S[i] - 'a';
+            int y = T[i] - 'a';
+            if (x != y) {
+                if (dis[x][y] == INF)
+                    return -1;
+                res += dis[x][y];
+            }
+        }
+        return res;
+    }
+};
