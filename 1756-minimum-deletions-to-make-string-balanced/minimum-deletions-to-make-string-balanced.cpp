e9eb5b1eb19e2edd515017e592cbd32dd72dc97f
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-07-30 11:56:47 +0530

    Time: 58 ms (99.04%) | Memory: 19.9 MB (99.68%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,19 +1,21 @@
+static const int _ = []() {ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); return 0;}();
+
 class Solution {
 public:
-  int minimumDeletions(string s) {
-    int ans = INT_MAX;
-    int pre[2] = {0, 0};
-    int suf[2] = {0, 0};
-    for (char ch : s) suf[ch - 'a']++;
-    for (int i = 0; i < s.size(); i++) {
-      // ithun b
-      suf[s[i]-'a']--;
-      int x = pre[1];
-      int y = suf[0];
-    //   cout << i <<" " << j <<" "
-      pre[s[i]-'a']++;
-      ans=min(ans,x+y);
+    int minimumDeletions(const string & s) const {
+        int ans = INT_MAX;
+        int pre[2] = {0, 0};
+        int suf[2] = {0, 0};
+        for (char ch : s)
+            suf[ch - 'a']++;
+        for (int i = 0; i < s.size(); i++) {
+            // ithun b
+            suf[s[i] - 'a']--;
+            int x = pre[1];
+            int y = suf[0];
+            pre[s[i] - 'a']++;
+            ans = min(ans, x + y);
+        }
+        return ans;
     }
-    return ans;
-  }
 };
