a87d3aafde1bbdf812918c941f3a460dc2154873
Aniket  Hend <119605156+AniketHend@users.noreply.github.com>
2024-10-05 19:04:21 +0530

    Time: 15 ms (60.57%) | Memory: 29.6 MB (9.28%) - LeetSync

--- 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.cpp
+++ 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.cpp
@@ -1,28 +1,45 @@
-class Solution {
-public:
-    int fun(int i, vector<int>& a, vector<int>& b, int mask,
-            vector<vector<int>>& dp) {
-        if (i == a.size()) {
-            return 0;
-        }
-        if (dp[i][mask] == -1) {
-            dp[i][mask] = INT_MAX;
-            for (int j = 0; j < a.size(); j++) {
-                if ((mask & (1 << j)) == 0) {
-                    dp[i][mask] = min(
-                        dp[i][mask],
-                        (a[i] ^ b[j]) + fun(i + 1, a, b, mask + (1 << j), dp));
-                }
-            }
-        }
+#include <bits/stdc++.h>
+using namespace std;
 
+#define ff first
+#define ss second
+#define pb push_back
+#define MOD 1000000007
+#define inf 1e18
+
+int solve(int i, int mask, vector<vector<int>>& dp, vector<int>& nums1,
+          vector<int>& nums2) {
+    if (i == nums1.size()) {
+        return 0;
+    }
+    if (dp[i][mask] != -1) {
         return dp[i][mask];
     }
+    int res = INT_MAX;
+    for (int k = 0; k < nums1.size(); k++) {
+        if ((mask & (1 << k)) == 0) { // jth element is not used till now
+            res = min(res, ((nums1[i] ^ nums2[k]) + solve(i + 1, mask | (1 << k),
+                                                        dp, nums1, nums2)));
+        }
+    }
+    return dp[i][mask] = res;
+}
 
+class Solution {
+public:
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
-        int mask = 0;
-        vector<vector<int>> dp(14, vector<int>((1 << 14), -1));
+        // since the given question hai n = 14 so i can apply the approach of dp
+        // with bitmasking yrr
 
-        return fun(0, nums1, nums2, mask, dp);
+        int n = nums1.size();
+        vector<vector<int>> dp(n + 1, vector<int>(1 << (n + 1), -1));
+        return solve(0, 0, dp, nums1, nums2);
     }
 };
+
+#ifdef LOCAL
+int32_t main() {
+    freopen("output.txt", "w", stdout);
+    cout << "Compiled!" << endl;
+}
+#endif
